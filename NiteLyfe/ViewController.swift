569cbe38271e11c140424818885cc7bdb7a8003b
Jonah Zukosky <jonah.zukosky@gmail.com>
2017-10-13 21:56:56 -0500

    Initial Commit

---
+++ NiteLyfe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NiteLyfe
+//
+//  Created by Jonah Zukosky on 10/13/17.
+//  Copyright © 2017 Jonah Zukosky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
