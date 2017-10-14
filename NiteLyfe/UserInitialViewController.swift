9a6d5f8fc4c29568202d2eff8840ba92b7233c9a
hacker290 <hacker290.archive.org>
2017-10-14 18:47:18 -0500

    multiple pins works now but really slow

--- NiteLyfe/UserInitialViewController.swift
+++ NiteLyfe/UserInitialViewController.swift
@@ -12,24 +12,54 @@ class UserInitialViewController: UIViewController {
 
     
     @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        let regionRadius:CLLocationDegrees = 2000
-        let initialLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(38.951705, -92.334072)
+        let regionRadius:CLLocationDegrees = 5000
+        let initialLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(38.946047, -92.330131)
+        //var artworks = [pinAnnotation]()
+        // Flat Branch, Shakespeares, Seoul taco, Tropical
+        var latitudes = [38.950148, 38.948679, 38.951276, 38.951848]
+         var longtitudes = [-92.332041, -92.327943, -92.3256, -92.331157]
         // 1000 meters: alittle more than half a mile
         mapView.setRegion(MKCoordinateRegionMakeWithDistance(initialLocation, regionRadius, regionRadius), animated: true)
-        
-        let myPin = pinAnnotation(title: "Tiger Hacks", subtitle:"Subtitle", coordinate: initialLocation)
-        
-        mapView.addAnnotation(myPin)
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        for index in 1...4{
+            if(index<5){
+                let my_latitude: Double = latitudes[index-1]
+                let my_longitude: Double = longtitudes[index-1]
+                // show artwork on map
+                let artwork = pinAnnotation(title: "King David Kalakaua",
+                                            locationName: "Waikiki Gateway Park",
+                                            discipline: "Sculpture",
+                                            coordinate: CLLocationCoordinate2D(latitude: my_latitude, longitude: my_longitude))
+                //artworks.append(artwork)
+                mapView.addAnnotation(artwork)
+            }
+        }
+        //mapView.addAnnotations(artworks)
+        super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+//    func loadInitialData() {
+//        // 1
+//        guard let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json")
+//            else { return }
+//        let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
+//
+//        guard
+//            let data = optionalData,
+//            let json = try? JSONSerialization.jsonObject(with: data),
+//            let dictionary = json as? [String: Any],
+//            let works = dictionary["data"] as? [[Any]]
+//            else { return }
+//        // 5
+//        let validWorks = works.flatMap { pinAnnotation(json: $0) }
+//        artworks.append(contentsOf: validWorks)
+//    }
 
     /*
     // MARK: - Navigation
@@ -40,5 +70,28 @@ class UserInitialViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+}
+extension UserInitialViewController: MKMapViewDelegate {
+    // 1
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // 2
+        guard let annotation = annotation as? pinAnnotation else { return nil }
+        // 3
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        // 4
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            // 5
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
 }
--- NiteLyfe/pinAnnotation.swift
+++ NiteLyfe/pinAnnotation.swift
@@ -8,14 +8,34 @@
 
 import MapKit
 
-class pinAnnotation: NSObject, MKAnnotation{
-    var title: String?
-    var subtitle: String?
-    var coordinate: CLLocationCoordinate2D
+class pinAnnotation: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
     
-    init(title:String,subtitle:String, coordinate:CLLocationCoordinate2D){
-        self.title=title
-        self.subtitle=subtitle
-        self.coordinate=coordinate
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    init?(json: [Any]) {
+        // 1
+        self.title = json[16] as? String ?? "No Title"
+        self.locationName = json[12] as! String
+        self.discipline = json[15] as! String
+        // 2
+        if let latitude = Double(json[18] as! String),
+            let longitude = Double(json[19] as! String) {
+            self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        } else {
+            self.coordinate = CLLocationCoordinate2D()
+        }
+    }
+    var subtitle: String? {
+        return locationName
     }
 }
